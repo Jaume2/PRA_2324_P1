90299f74b83690e259be45e17213c88b2a27c0aa
Jaume Gimeno Ponz <jgimpon@upv.edu.es>
2025-11-11 18:32:48 +0100

    Update ListLinked.h

--- ListLinked.h
+++ ListLinked.h
@@ -44,22 +44,27 @@ public:
 		out << list->first->data;
 		return out;
 	}
-	void insert(int pos, T e) override
-	{
-		if (pos < 0 || pos > n)
-		{
-			throw std::out_of_range("Posición fuera de rango");
+	void insert(int pos, T e) override{
+		if(pos < 0 || pos > n) throw std::out_of_range("Posición fuera del rango de la array");
+		if(pos == 0){
+			Node<T>* aux = new Node<T>(e,first); 
+			first = aux;
+			n++;
 		}
-		if (n == max)
-		{
-			resize(max * 2);
+		else{
+			Node<T>* prev = nullptr;
+			Node<T>* aux = first;
+			int i = 0;
+			while(aux != nullptr && i < pos){
+				prev = aux;
+				aux = aux -> next;
+				i++;
+			}
+			if(i == pos){
+				prev->next = new Node<T>(e,aux); 
+				n++;
+			}	
 		}
-		for (int i = n; i > pos; --i)
-		{
-			arr[i] = arr[i - 1];
-		}
-		arr[pos] = e;
-		++n;
 	}
 	void append(T e) override
 	{
@@ -87,24 +92,24 @@ public:
 		}
 		return removed_value;
 	}
-	T get(int pos) override
-	{
-		if (pos < 0 || pos >= n)
-		{
-			throw std::out_of_range("Posición fuera de rango");
-		}
-		return arr[pos];
+	T get(int pos) override{
+		if(pos < 0 || pos >= n) throw std::out_of_range("Posición fuera del rango de la array");
+		Node<T>* aux = first;
+		for(int i= 0; i < pos; i++) aux = aux->next;
+		return aux->data;
 	}
-	int search(T e) override
-	{
-		for (int i = 0; i < n; ++i)
-		{
-			if (arr[i] == e)
-			{
-				return i;
-			}
+	int search(T e) override {
+        Node<T>* aux = first;
+		int i = 0;
+		while(aux != nullptr && aux-> data != e){
+			aux = aux-> next;
+			i++;
 		}
-		return -1;
+		if(aux != nullptr){ 
+			return i;
+		}else{ 
+			return -1;
+        }
 	}
 	bool empty() override
 	{
@@ -113,13 +118,5 @@ public:
 	int size() override
 	{
 		return n;
-	}
-	T operator[](int pos)
-	{
-		if (pos < 0 || pos >= n)
-		{
-			throw out_of_range("Posición fuera de rango");
-		}
-		return arr[pos];
 	}
 };
